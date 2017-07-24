61676c75272650d151b9d1cac4702d7aabb5ab82
Albert Bertram <bertrama@umich.edu>
2017-07-24 15:40:54 -0400

    Add FormattedFullTitleFiled type.

--- lib/spectrum/config.rb
+++ lib/spectrum/config.rb
@@ -51,6 +51,7 @@ require 'spectrum/config/summon_date_field'
 require 'spectrum/config/formatted_catalog_published_field'
 require 'spectrum/config/formatted_article_published_field'
 require 'spectrum/config/formatted_page_range_field'
+require 'spectrum/config/formatted_full_title_field'
 require 'spectrum/config/field_list'
 
 require 'spectrum/config/facet'
---
+++ lib/spectrum/config/formatted_full_title_field.rb
@@ -0,0 +1,33 @@
+module Spectrum
+  module Config
+    class FormattedFullTitleField < Field
+      type "formatted_full_title"
+
+      attr_reader :fields
+      def initialize_from_instance(i)
+        super
+        @fields = i.fields
+      end
+
+      def initialize_from_hash(args, config)
+        super
+        @fields = {}
+        args['fields'].each_pair do |fname, fdef|
+          @fields[fname] = Field.new(
+            fdef.merge({'id' => SecureRandom.uuid, 'metadata' => {}}),
+            config
+          )
+        end
+      end
+
+      def value(data)
+        @fields['title'].value(data).map do |row|
+          kv = row.inject({}) {|acc, i| acc[i[:uid]] = Array(i[:value]).join(' ') ; acc }
+          [kv['first'], kv['last']].compact.join(': ')
+        end
+      end
+
+    end
+  end
+end
+    
