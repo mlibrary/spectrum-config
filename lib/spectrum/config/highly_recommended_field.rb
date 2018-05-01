68d9c6e23dd4d60a9b9ad7348e5d2f13a685c1d9
Albert Bertram <bertrama@umich.edu>
2018-05-01 16:15:39 -0400

    Bugfix

--- lib/spectrum/config/highly_recommended_field.rb
+++ lib/spectrum/config/highly_recommended_field.rb
@@ -5,7 +5,7 @@ module Spectrum
       type 'highly_recommended'
 
       def value(data, request = nil)
-        return nil unless request
+        return nil unless request && request.respond_to?(:facets)
         topics = request.facets.data[facet_field]&.select do |value|
           key = field + value.downcase.gsub(/[^a-z&]/, '_').gsub(/_+/, '_').sub(/_+$/, '')
           data[key].to_i < 100
