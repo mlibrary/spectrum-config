a579b3c1c628d7ec3c4b6986367be6345fdec62c
Albert Bertram <bertrama@umich.edu>
2019-07-05 14:22:25 -0400

    Add support for selecting fields based on the value of a subfield.

--- lib/spectrum/config/marc_matcher.rb
+++ lib/spectrum/config/marc_matcher.rb
@@ -18,13 +18,13 @@ module Spectrum
         @sub   = /#{arg['sub'] || '.'}/
         @ind1  = /#{arg['ind1'] || '.'}/
         @ind2  = /#{arg['ind2'] || '.'}/
+        @where = arg['where']
         @default = arg['default']
       end
 
       def match_field(field)
         if @tag.match(field.tag)
-          return true unless field.respond_to?(:indicator1) && field.respond_to?(:indicator2)
-          @ind1.match(field.indicator1) && @ind2.match(field.indicator2)
+          match_indicators(field) && match_where(field)
         else
           false
         end
@@ -32,6 +32,34 @@ module Spectrum
 
       def match_subfield(subfield)
         @sub.match(subfield.code)
+      end
+
+      def match_indicators(field)
+        return true unless field.respond_to?(:indicator1) && field.respond_to?(:indicator2)
+        @ind1.match(field.indicator1) && @ind2.match(field.indicator2)
+      end
+
+      def match_where(field)
+        return true unless @where
+        return true unless field.respond_to?(:find_all)
+        @where.all? do |clause|
+          if clause['not']
+            ret = false
+            values = [clause['not']].flatten
+          elsif clause['is']
+            ret = true
+            values = [clause['is']].flatten
+          end
+          subfields = field.find_all do |subfield|
+            /#{clause['sub']}/.match(subfield.code) &&
+              values.include?(subfield.value)
+          end
+          if subfields.length > 0
+            ret
+          else
+            !ret
+          end
+        end
       end
     end
   end
