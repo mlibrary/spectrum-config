aeba8bd14c4a60161491e3c7b50acc2dd4fd7551
Albert Bertram <bertrama@umich.edu>
2020-03-01 18:31:27 -0500

    Add DelimitedSearch1MetadataComponent.

--- lib/spectrum/config/delimited_search1_metadata_component.rb
+++ lib/spectrum/config/delimited_search1_metadata_component.rb
@@ -1,28 +1,31 @@
 module Spectrum
   module Config
-    class Search1MetadataComponent < MetadataComponent
-      type 'search1'
+    class DelimitedSearch1MetadataComponent < MetadataComponent
+      type 'delimited_search1'
 
-      attr_accessor :name, :variant, :scope
+      attr_accessor :name, :variant, :scope, :delimiter
 
       def initialize(name, config)
         config ||= {}
         self.name = name
         self.scope = config['scope']
         self.variant = config['variant']
+        self.delimiter = config['delimiter']
       end
 
       def resolve_description(data)
-        [data].flatten(1).map { |item|
-          {
-            text: item,
-            search: {
-              type: variant,
-              scope: scope,
-              value: item,
+        [data].flatten(1).map do |list|
+          list.to_s.split(delimiter).reject(&:empty?).map do |item|
+            {
+              text: item,
+              search: {
+                type: variant,
+                scope: scope,
+                value: item,
+              }
             }
-          }
-        }.compact
+          end
+        end.compact
       end
 
       def resolve(data)
