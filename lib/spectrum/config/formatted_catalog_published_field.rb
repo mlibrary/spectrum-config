a3667271076256c0943e62bf97742e61ec6b73ba
Albert Bertram <bertrama@umich.edu>
2020-03-26 16:27:21 -0400

    Reformatting catalog published field.

--- lib/spectrum/config/formatted_catalog_published_field.rb
+++ lib/spectrum/config/formatted_catalog_published_field.rb
@@ -4,35 +4,60 @@ module Spectrum
     class FormattedCatalogPublishedField < Field
       type 'formatted_catalog_published'
 
-      attr_reader :fields
+      attr_reader :date, :groups
       def initialize_from_instance(i)
         super
-        @fields = i.fields
+        @date = i.date
+        @groups = i.groups
       end
 
       def initialize_from_hash(args, config)
         super
-        @fields = {}
-        args['fields'].each_pair do |fname, fdef|
-          @fields[fname] = Field.new(
+        @groups = args['groups'].map do |fdef|
+          Field.new(
             fdef.merge('id' => SecureRandom.uuid, 'metadata' => {}),
             config
           )
         end
+        @date = Field.new(
+          args['date'].merge('id' => SecureRandom.uuid, 'metadata' => {}),
+          config
+        )
+      end
+
+      def get_date_val(data)
+        date_val = [@date.value(data)].flatten.first
+        return date_val.strip unless date_val.nil?
+        date_val
+      end
+
+      def get_pub_vals(data)
+        date_val = get_date_val(data)
+
+        @groups.map do |group|
+          values = [group.value(data)].flatten(1).reject do |item|
+            item.nil? || item.empty?
+          end.map do |item|
+            if item.any? {|fl| fl[:uid] == 'c'}
+              [
+                (item.find {|fl| fl[:uid] == 'ab'} || {})[:value]
+              ].flatten(1).reject do |val|
+                val.nil? || !(String === val) || val.empty?
+              end.map do |val|
+                [date_val, val].compact.join(' - ')
+              end
+            else
+              (item.find {|fl| fl[:uid] == 'ab'} || {})[:value]
+            end
+          end.flatten
+          values.empty? ? nil : values
+        end.flatten.compact
       end
 
       def value(data, request = nil)
-        date = [@fields['pub_date'].value(data)].flatten.first
-        date = date.strip unless date.nil?
-        pub  = [@fields['publisher'].value(data)].flatten.reject do |item|
-          item.nil? || item.empty?
-        end.map do |item|
-          [date, item].reject do |str|
-            str.nil? || !(String === str) || str.empty?
-          end.join(' - ')
-        end
-        return nil if pub.empty?
-        pub
+        pub_vals = get_pub_vals(data)
+        return nil if pub_vals.empty?
+        pub_vals
       end
     end
   end
