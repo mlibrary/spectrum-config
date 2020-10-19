9db5c2c98cb51df01cd21ab9cc9145714acefc12
Albert Bertram <bertrama@umich.edu>
2020-10-19 14:58:21 -0400

    SEARCH-649: Add a join argument to the 'concat' field type.

--- lib/spectrum/config/concat_field.rb
+++ lib/spectrum/config/concat_field.rb
@@ -4,8 +4,22 @@ module Spectrum
     class ConcatField < Field
       type 'concat'
 
+      attr_reader :join
+
+      def initialize_from_instance(i)
+        super
+        @join = i.join
+      end
+
+      def initialize_from_hash(args, config = {})
+        super
+        @join = args['join'] || ''
+      end
+
       def value(data, request = nil)
-        @field.map { |name| resolve_key(data, name) }.join('')
+        val = @field.map { |name| resolve_key(data, name) }.flatten.compact.join(@join)
+        return nil if val.nil? || val.empty?
+        val
       end
     end
   end
