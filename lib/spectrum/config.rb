81a8d060a4101f1bc8a356f3aa1aed839f6f9923
Albert Bertram <bertrama@umich.edu>
2021-06-21 16:28:53 -0400

    Add a class for PlaceholderHeaderComponent.

--- lib/spectrum/config.rb
+++ lib/spectrum/config.rb
@@ -56,6 +56,8 @@ require 'spectrum/config/quoted_search2_metadata_component'
 require 'spectrum/config/header_region'
 require 'spectrum/config/header_component'
 require 'spectrum/config/plain_header_component'
+require 'spectrum/config/icon_header_component'
+require 'spectrum/config/placeholder_header_component'
 
 require 'spectrum/config/highly_recommended'
 require 'spectrum/config/sort'
